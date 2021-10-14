77203ee5aacd17dcadb463fe72b718fd85fc60d7
JacksonDonaldson <66980702+JacksonDonaldson@users.noreply.github.com>
2021-10-14 18:35:51 -0400

    Update listener.py

--- listener.py
+++ listener.py
@@ -7,8 +7,6 @@ import dataStorage
 import sys
 import emailChecker
 import imaplib
-# email = "mopavet284@ddwfzp.com"
-# password = "thisisatestpassword"
 
 #url = "https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440"
 cookie = "dtSa=-"
